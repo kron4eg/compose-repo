fd96add83f8afd6ff93421e5d740dd3484882281
Artiom Diomin <kron82@gmail.com>
2017-04-19 12:07:08 +0300

    update brew formula

--- compose-repo.rb
+++ compose-repo.rb
@@ -3,9 +3,9 @@ require 'formula'
 class ComposeRepo < Formula
   desc "syncronize your docker-compose repos and rependancies"
   homepage "https://github.com/kron4eg/compose-repo"
-  url "https://github.com/kron4eg/compose-repo/releases/download/v0.0.5/compose-repo_darwin_amd64.zip"
-  version "0.0.5"
-  sha256 "20aa62198429f02e7d5f7a6530c8a6efdd2af0c036d9745507d32a40d85f5603"
+  url "https://github.com/kron4eg/compose-repo/releases/download/v0.0.6/compose-repo_darwin_amd64.zip"
+  version "0.0.6"
+  sha256 "ec93caeaf6b8ed64a8fcb33ac4c361ab19cfbc062ee770d578863d1e20c30ff9"
   depends_on :arch => :x86_64
 
   def install
